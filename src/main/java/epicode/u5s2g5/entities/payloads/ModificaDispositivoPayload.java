db0874eb0dc26da2861a90175451ef773a6b2333
Luigi160397 <luigi.difraia.97@gmail.com>
2023-06-09 14:57:28 +0200

    esercizio5

---
+++ src/main/java/epicode/u5s2g5/entities/payloads/ModificaDispositivoPayload.java
@@ -0,0 +1,18 @@
+package epicode.u5s2g5.entities.payloads;
+
+import java.util.UUID;
+
+import epicode.u5s2g5.entities.StatoDispositivo;
+import epicode.u5s2g5.entities.TipoDispositivo;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ModificaDispositivoPayload {
+	@NotNull(message = "Il tipo del dispositivo è obbligatorio")
+	TipoDispositivo tipoDispositivo;
+	@NotNull(message = "Lo stato del dispositivo è obbligatorio")
+	StatoDispositivo statoDispositivo;
+	@NotNull(message = "Lo user-id del dispositivo è obbligatorio")
+	UUID idUtente;
+}
