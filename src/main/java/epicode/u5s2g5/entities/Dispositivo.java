93fe095794d3573a955ba93c7381ccd774f1ebee
Luigi160397 <luigi.difraia.97@gmail.com>
2023-06-09 17:44:25 +0200

    json reference

--- src/main/java/epicode/u5s2g5/entities/Dispositivo.java
+++ src/main/java/epicode/u5s2g5/entities/Dispositivo.java
@@ -2,6 +2,8 @@ package epicode.u5s2g5.entities;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -29,6 +31,7 @@ public class Dispositivo {
 	@Enumerated(EnumType.STRING)
 	private StatoDispositivo statoDispositivo;
 	@ManyToOne
+	@JsonBackReference
 	private User user;
 
 	public Dispositivo(TipoDispositivo tipo, StatoDispositivo statoDispositivo, User user) {
